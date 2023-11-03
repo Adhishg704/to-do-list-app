e4070b9e5cc7b55af797650534f4e18885702ca4
AD <adhish.g704@gmail.com>
2023-11-03 16:38:15 +0530

    first commit

---
+++ src/components/UserListForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function UserListForm() {
+  const [Note, setNote] = useState("");
+  const [Notes, setNotes] = useState([]);
+
+  function handleNoteChange(event) {
+    const { value: newValue } = event.target;
+    setNote(() => {
+        return(
+            newValue
+        );
+    });
+  }
+
+  function handleClick() {
+    setNotes((prevNotes) => {
+      return [...prevNotes, Note];
+    });
+    setNote(() => {
+        return "";
+    }); 
+  }
+
+  return (
+    <div>
+        <div className="form">
+            <input type="text" onChange={handleNoteChange} value={Note} />
+            <button onClick={handleClick}>
+                <span>Add</span>
+            </button>
+        </div>
+        <div>
+            <ul>
+                {
+                    Notes.map((note) => {
+                        return(
+                            <li>{note}</li>
+                        );
+                    })
+                }
+            </ul>
+      </div>
+    </div>
+    
+  );
+}
+
+export default UserListForm;
